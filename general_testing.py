93de10eee3d41818d6740c1ab7c58d564a05b88b
DiabloXTREME <thediabloxtreme666@gmail.com>
2024-05-23 11:21:04 +0530

    Minor change, moving the API testing to general_testing.py

--- general_testing.py
+++ general_testing.py
@@ -1,2 +1,21 @@
+import os
+from api_testing import API_Tester, createNewAPI
+from dataManager import dataManager
+
 # Colored Output Testing
 print(f"Error in\033[94m Class\033[0m, writeToFile function: \033[91m \nerror")  # Success
+
+# Full test of report
+api = createNewAPI("apis/streaming_availability.json")
+query = {
+    "country": "in",
+    "show_type": "series",
+    "genres": "action, comedy",
+    "order_by": "rating",
+    "order_direction": "desc",
+    "genres_relation": "and"
+}
+api.query(query)
+manager = dataManager(api)
+manager.createReport()
+
